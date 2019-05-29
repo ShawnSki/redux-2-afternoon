3302778d89493d581c98bdc72b131b4b16bb3b65
shawn's macbook <shawnscivally@shawn-ski.local>
2019-05-29 16:09:54 -0600

    done

--- src/store.js
+++ src/store.js
@@ -1,12 +1,11 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
 import budgetReducer from './ducks/budgetReducer';
 import userReducer from './ducks/userReducer';
 
-
 const rootReducer = combineReducers({
-    budget: budgetReducer,
-    user: userReducer
+  budget: budgetReducer,
+  user: userReducer
 })
 
-export default createStore(rootReducer, applyMiddleware(promiseMiddleware))
+export default createStore(rootReducer, applyMiddleware(promiseMiddleware));
